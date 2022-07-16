323c3402360db30eb6f87ffa6ddf8cca7515ff21
Nishit <nishitranjan41@gmail.com>
2022-07-16 22:53:33 +0530

    this is Employee Attendence msg

--- src/com/bridgelabz/basics/EmployeeWage.java
+++ src/com/bridgelabz/basics/EmployeeWage.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.basics;
 
 public class EmployeeWage {
+    static  final int EMPLOYEE_PRESENT=1;
+
     public static void main(String[] args) {
         System.out.println("welcome to employee wages");
+        double empCheck=Math.floor(Math.random()*10) % 2;
+        if(empCheck==EMPLOYEE_PRESENT) {
+            System.out.println("employee is present");
+        }
+        else {
+            System.out.println("employee is absent");
     }
 }

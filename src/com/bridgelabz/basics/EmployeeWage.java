721427c65c27600647bcdcc39b4fa99afa5871a2
Nishit <nishitranjan41@gmail.com>
2022-07-18 06:57:00 +0530

    this is for daily wages

--- src/com/bridgelabz/basics/EmployeeWage.java
+++ src/com/bridgelabz/basics/EmployeeWage.java
@@ -1,15 +1,20 @@
 package com.bridgelabz.basics;
 
 public class EmployeeWage {
-    static  final int EMPLOYEE_PRESENT=1;
+    static final int EMPLOYEE_PRESENT = 1;
+    static final int IS_PARTTIME = 4;
+    static final int FULLDAYHOUR = 8;
+    static final int FULLDAYWAGESPERHR = 20;
 
     public static void main(String[] args) {
         System.out.println("welcome to employee wages");
-        double empCheck=Math.floor(Math.random()*10) % 2;
-        if(empCheck==EMPLOYEE_PRESENT) {
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == EMPLOYEE_PRESENT) {
             System.out.println("employee is present");
-        }
-        else {
+            int dailyWages = FULLDAYHOUR * FULLDAYWAGESPERHR;
+            System.out.println("Daily wages is=>" + dailyWages);
+        } else {
             System.out.println("employee is absent");
+        }
     }
 }
